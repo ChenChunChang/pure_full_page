6ed206e3a8e3104cf5ba6a735f05a8bd8437f3f4
liuxiaoguang <vincexgliu@gmail.com>
2018-05-02 01:50:04 +0800

    update demo

--- js/index.js
+++ js/index.js
@@ -1,27 +1,21 @@
-/**
- * 添加交互动画
- */
-var addAnimation = function(currentPosition) {
-  var pages = document.querySelectorAll('.page');
-  pages = Array.prototype.slice.call(pages);
-  var viewHeight = document.documentElement.clientHeight;
-  // i 表示当前页面的索引
-  var i = -(currentPosition / viewHeight);
+document.addEventListener('DOMContentLoaded', function() {
+  // 添加交互动画,不能使用箭头函数，要引用实例中的 this
+  var addAnimation = function() {
+    // i 表示每次滑动将要进入的页面的索引，可以通过 this.pages[i] 获取当前页面
+    // 取得将要进入页面后便可以做进一步操作，比如，添加动画
+    var i = -(this.currentPosition / this.viewHeight);
 
-  /**
-   * TODO：每次进入当前页面都会执行下面代码，
-   * 修改下面代码，可自定义页面动画
-   * 下面示例给每个页面的 p 元素添加了淡入动画
-   */
-  document.querySelector('.fade-in').classList.remove('fade-in');
-  pages[i].querySelector('p').classList.add('fade-in');
-};
+    // 为将要进入页面添加动画
+    document.querySelector('.fade-in').classList.remove('fade-in');
+    this.pages[i].querySelector('p').classList.add('fade-in');
+  };
 
-// 创建全屏滚动实例，传入动画回调函数，并初始化
-new PureFullPage({
-  definePages: addAnimation,
-}).init();
+  // 创建全屏滚动实例，传入动画回调函数，并初始化
+  new PureFullPage({
+    definePages: addAnimation,
+  }).init();
+});
 
-window.onload = function() {
+window.addEventListener('load', function() {
   document.querySelector('p').classList.add('fade-in');
-};
+});
